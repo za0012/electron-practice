059e2c03e5e1a855eeb57c0335e1454cc2bb3031
za0012 <zinazeek@gmail.com>
2026-03-12 11:19:33 +0900

    🔧chore: Path Alias추가, plugins에 tanstack router, tailwind 추가

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,16 +1,28 @@
-import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "electron-vite";
+import { fileURLToPath, URL } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   main: {},
   preload: {},
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+        "@renderer": resolve("src/renderer/src"),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-    plugins: [react()]
-  }
-})
+    plugins: [
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: true,
+      }),
+      react(),
+      tailwindcss(),
+    ],
+  },
+});
